8ffc40186fcc26f6cf18f1e96428ae0240b75260
John Maxwell <jmax@jmaxhome.com>
2022-01-26 19:51:21 -0500

    Rationalized spec/ tree and cleaned cruft out of Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,16 +16,12 @@ module.exports = function(grunt) {
           consolidate: true
         }
       },
-        all: {specNameMatcher: ['spec/obj/*_spec.js', 'spec/new/*_spec.js']},
-      quick: {specNamMatcher: 'spec/obj/vector_spec.js'}
+        all: {specNameMatcher: 'spec/**/*_spec.js'}
     },
 
     jshint: {
       all: ['src/**/*.js'],
       options: {
-        // globals: {
-        //   _: false
-        // },
         browser: true,
         devel: true,
         reporterOutput: ""
@@ -43,8 +39,6 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-jasmine-node');
-  grunt.loadNpmTasks('grunt-contrib-clean');
 
   grunt.registerTask('default', ['jshint', 'jasmine_node:all']);
-  grunt.registerTask('quick',   ['jshint', 'jasmine_node:quick']);
 };
