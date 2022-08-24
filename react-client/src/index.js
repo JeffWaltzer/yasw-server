a2a9d72f3f0a5a62a02de5a9117b5a6934deee2b
John Maxwell <jmax@jmaxhome.com>
2022-08-23 20:56:33 -0400

    Spike client appears to connect.

--- react-client/src/index.js
+++ react-client/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -11,6 +12,18 @@ root.render(
   </React.StrictMode>
 );
 
+let exampleSocket;
+try {
+  exampleSocket = new WebSocket(`ws://${window.location.host}/engine.io/?EIO=3&transport=websocket`);
+  exampleSocket.onopen = (event) => {
+    console.log(`onopen: event: ${event}`);
+
+    exampleSocket.send("ferd is a ferd");
+  };
+}
+catch (e) {
+  console.log(`error: ${e}`);
+}
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
