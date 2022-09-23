cf609ae13ce69adc3b058d7381c2340838f11f94
John Maxwell <jmax@jmaxhome.com>
2022-09-22 20:54:42 -0400

    First real spec
    An empty JSON object renders as an empty SVG tag

--- react-client/src/App.test.js
+++ react-client/src/App.test.js
@@ -9,42 +9,9 @@ configure({ adapter: new Adapter() });
 
 describe("<App />", () => {
   it('renders learn react link', () => {
-    let screen= shallow(<App />);
+    let screen= shallow(<App gameboard="{}"/>);
 
-    console.log(`App(): ${JSON.stringify(App(), null, 4)}`);
-    console.log(`screen: ${JSON.stringify(screen, null, 4)}`);
-
-    expect(screen.find("div")).to.have.lengthOf(1);
-
-    // const linkElement = screen.getByText(/learn react/i);
-    // expect(linkElement).toBeInTheDocument();
+    let svg_tags = screen.find("svg")
+    expect(svg_tags.length).toEqual(1);
   });
 });
-
-// describe('<App />', () => {
-//   it('renders three <Foo /> components', () => {
-//     const wrapper = shallow(<MyComponent />);
-//     expect(wrapper.find(Foo)).to.have.lengthOf(3);
-//   });
-
-//   it('renders an `.icon-star`', () => {
-//     const wrapper = shallow(<MyComponent />);
-//     expect(wrapper.find('.icon-star')).to.have.lengthOf(1);
-//   });
-
-//   it('renders children when passed in', () => {
-//     const wrapper = shallow((
-//       <MyComponent>
-//         <div className="unique" />
-//       </MyComponent>
-//     ));
-//     expect(wrapper.contains(<div className="unique" />)).to.equal(true);
-//   });
-
-//   it('simulates click events', () => {
-//     const onButtonClick = sinon.spy();
-//     const wrapper = shallow(<Foo onButtonClick={onButtonClick} />);
-//     wrapper.find('button').simulate('click');
-//     expect(onButtonClick).to.have.property('callCount', 1);
-//   });
-// });
