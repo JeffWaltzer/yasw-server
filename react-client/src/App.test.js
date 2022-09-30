9116583f8ee85a4d2db5c6f35adb29e392a38167
Jeff Waltzer <jeffwaltzer@gmail.com>
2022-09-29 21:05:03 -0400

    (failing spec) We need to try SVG next

--- react-client/src/App.test.js
+++ react-client/src/App.test.js
@@ -1,17 +1,92 @@
-// import { render, screen } from '@testing-library/react';
+// // import { render, screen } from '@testing-library/react';
+//
+// import { configure, mount, shallow  } from 'enzyme';
+//
+// import sinon from 'sinon';
+// import App from './App';
+//
+// import Adapter from 'enzyme-adapter-react-17';
+// configure({ adapter: new Adapter() });
+//
+// describe("<App />", () => {
+// const the_sun={
+//   "polygons": [
+//     {
+//       "wireframe": [
+//         {
+//           "points": [
+//             [
+//               512,
+//               532
+//             ],
+//             [
+//               526,
+//               526
+//             ],
+//             [
+//               532,
+//               512
+//             ],
+//             [
+//               526,
+//               498
+//             ],
+//             [
+//               512,
+//               492
+//             ],
+//             [
+//               498,
+//               498
+//             ],
+//             [
+//               492,
+//               512
+//             ],
+//             [
+//               498,
+//               526
+//             ]
+//           ],
+//           "color": "orange"
+//         }
+//       ],
+//       "score": 0,
+//       "position": [
+//         512,
+//         512
+//       ]
+//     },
+//   ],
+//   "field_size": [
+//     1024,
+//     1024
+//   ]
+// }
+//
+//   it('renders learn react link', () => {
+//     let screen= shallow(<App gameboard="{}"/>);
+//
+//     let svg_tags = screen.find("svg")
+//     expect(svg_tags.length).toEqual(1);
+//   });
+//
+//   it('renders the sun', () => {
+//     let screen= mount(<App gameboard="{JSON.stringify(the_sun)}"/>);
+//
+//     let svg_tags = screen.find("polygon")
+//     expect(svg_tags.length).toEqual(1);
+//
+//   })
+// });
+import {render, screen} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import '@testing-library/jest-dom'
+import App from './App'
 
-import { configure, shallow } from 'enzyme';
-import sinon from 'sinon';
-import App from './App';
+it('loads and displays greeting', async () => {
+    const screen = render(<App url="/greeting" />)
 
-import Adapter from 'enzyme-adapter-react-16';
-configure({ adapter: new Adapter() });
-
-describe("<App />", () => {
-  it('renders learn react link', () => {
-    let screen= shallow(<App gameboard="{}"/>);
-
-    let svg_tags = screen.find("svg")
-    expect(svg_tags.length).toEqual(1);
-  });
-});
+     let svg_tags = screen.getByRole("svg")
+     expect(svg_tags.length).toEqual(1);
+})
