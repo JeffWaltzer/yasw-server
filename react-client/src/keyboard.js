8cb81355d74b5c8aa13649a7ebe17cd6f9107539
John Maxwell <jmax@jmaxhome.com>
2023-02-23 21:02:22 -0500

    Started on keyboard logic. *BROKEN TEST*

--- react-client/src/keyboard.js
+++ react-client/src/keyboard.js
@@ -1,6 +1,8 @@
 export default class  Keyboard {
   constructor() {
+    this.left_key = "up";
+    this.right_key = "up";
+    this.thrust_key = "up";
+    this.fire_key = "up";
   }
-}
-
-;
+};
--- react-client/src/test/ship_command.test.js
+++ react-client/src/test/ship_command.test.js
@@ -1,17 +1,503 @@
 import  Keyboard  from '../keyboard';
+import  GameServer  from '../game_server';
 
-describe("keyboard", () => {
+describe("Keyboard", () => {
+  let game_server;
   let keyboard;
-  beforeEach(() => {
-    keyboard=new Keyboard();
-  });
-  describe("Initial key states", () =>{
 
-      it("start left up", function() {
-        return expect(keyboard.left_key).toBe("up");
+  beforeEach(() => {
+    game_server = jest.spyOn(GameServer, 'send');
+    keyboard = new Keyboard();
+  });
+
+  describe("Initial key states", () => {
+    it("start left up", () => {
+      expect(keyboard.left_key).toBe("up");
+    });
+    it("start right up", () => {
+      expect(keyboard.right_key).toBe("up");
+    });
+    it("start thrust up", () => {
+      expect(keyboard.thrust_key).toBe("up");
+    });
+    it("start fire up", () => {
+      expect(keyboard.fire_key).toBe("up");
+    });
+  });
+
+  const fire_up_sent_tests = [
+    {
+      fire_key: "up",
+      expected_sent: null
+    },
+    {
+      fire_key: "down",
+      expected_sent: null
+    }
+  ];
+  fire_up_sent_tests.forEach(function(test_conditions) {
+    describe(`When fire key is ${test_conditions.fire_key}`, () => {
+      describe(" and we receive up", () => {
+        if (test_conditions.expected_sent) {
+          it(`sends ${test_conditions.expected_sent}`, () => {
+            expect(game_server.send).toHaveBeenCalledWith(test_conditions.expected_sent);
+          });
+        } else {
+          it("does not send", () => {
+            expect(game_server.send).not.toHaveBeenCalled();
+          });
+        }
       });
-  })
+    });
+  });
+
+  const fire_down_sent_tests = [
+    {
+      fire_key: "up",
+      expected_sent: 'fire'
+    },
+    {
+      fire_key: "down",
+      expected_sent: null
+    }
+  ];
+  fire_down_sent_tests.forEach(function(test_conditions) {
+    describe(`When fire key is ${test_conditions.fire_key}`, () => {
+      describe(" and we receive down", () => {
+        if (test_conditions.expected_sent) {
+          it(`sends ${test_conditions.expected_sent}`, () => {
+            // expect(game_server.send).toHaveBeenCalledWith(test_conditions.expected_sent);
+          });
+        } else {
+          it("does not send", () => {
+            // expect(game_server.send).not.toHaveBeenCalled();
+          });
+        }
+      });
+    });
+  });
+
+  const fire_up_state_tests = [
+    {
+      fire_key: "up",
+      expected_state: "up"
+    },
+    {
+      fire_key: "down",
+      expected_state: "up"
+    }
+  ];
+  fire_up_state_tests.forEach(function(test_conditions) {
+    describe(`When fire key is ${test_conditions.fire_key}`, () => {
+      describe(" and we receive up", () => {
+        it(`fire key is ${test_conditions.expected_state}`, () => {
+          // expect(keyboard.fire_key).toEqual(test_conditions.expected_state);
+        });
+      });
+    });
+  });
+  const fire_down_state_tests = [
+    {
+      fire_key: "up",
+      expected_state: "down"
+    },
+    {
+      fire_key: "down",
+      expected_state: "down"
+    }
+  ];
+  fire_down_state_tests.forEach(function(test_conditions) {
+    describe(`When fire key is ${test_conditions.fire_key}`, () => {
+      describe(" and we receive key_down", () => {
+        it(`fire key is ${test_conditions.expected_state}`, () => {
+          // expect(keyboard.fire_key).toEqual(test_conditions.expected_state);
+        });
+      });
+    });
+  });
+  const thrust_up_sent_tests = [
+    {
+      thrust_key: "up",
+      expected_sent: null
+    },
+    {
+      thrust_key: "down",
+      expected_sent: 'thrust_off'
+    }
+  ];
+  thrust_up_sent_tests.forEach(function(test_conditions) {
+    describe(`When thrust key is ${test_conditions.thrust_key}`, () => {
+      describe(" and we receive up", () => {
+        if (test_conditions.expected_sent) {
+          it(`sends ${test_conditions.expected_sent}`, () => {
+            // expect(game_server.send).toHaveBeenCalledWith(test_conditions.expected_sent);
+          });
+        } else {
+          it("does not send", () => {
+            // expect(game_server.send).not.toHaveBeenCalled();
+          });
+        }
+      });
+    });
+  });
+  const thrust_down_sent_tests = [
+    {
+      thrust_key: "up",
+      expected_sent: 'thrust_on'
+    },
+    {
+      thrust_key: "down",
+      expected_sent: null
+    }
+  ];
+  thrust_down_sent_tests.forEach(function(test_conditions) {
+    describe(`When thrust key is ${test_conditions.thrust_key}`, () => {
+      describe(" and we receive down", () => {
+        if (test_conditions.expected_sent) {
+          it(`sends ${test_conditions.expected_sent}`, () => {
+            // expect(game_server.send).toHaveBeenCalledWith(test_conditions.expected_sent);
+          });
+        } else {
+          it("does not send", () => {
+            // expect(game_server.send).not.toHaveBeenCalled();
+          });
+        }
+      });
+    });
+  });
+  const thrust_up_state_tests = [
+    {
+      thrust_key: "up",
+      expected_state: "up"
+    },
+    {
+      thrust_key: "down",
+      expected_state: "up"
+    }
+  ];
+  thrust_up_state_tests.forEach(function(test_conditions) {
+    describe(`When thrust key is ${test_conditions.thrust_key}`, () => {
+      describe(" and we receive up", () => {
+        it(`thrust key is ${test_conditions.expected_state}`, () => {
+          // expect(keyboard.thrust_key).toEqual(test_conditions.expected_state);
+        });
+      });
+    });
+  });
+  const thrust_down_state_tests = [
+    {
+      thrust_key: "up",
+      expected_state: "down"
+    },
+    {
+      thrust_key: "down",
+      expected_state: "down"
+    }
+  ];
+  thrust_down_state_tests.forEach(function(test_conditions) {
+    describe(`When thrust key is ${test_conditions.thrust_key}`, () => {
+      describe(" and we receive key_down", () => {
+        it(`thrust key is ${test_conditions.expected_state}`, () => {
+          // expect(keyboard.thrust_key).toEqual(test_conditions.expected_state);
+        });
+      });
+    });
+  });
+  const up_sent_tests = [
+    {
+      left_key: "down",
+      right_key: "down",
+      event: "right",
+      expected_sent: "rotate_left"
+    },
+    {
+      left_key: "down",
+      right_key: "down",
+      event: "left",
+      expected_sent: "rotate_right"
+    },
+    {
+      left_key: "up",
+      right_key: "down",
+      event: "right",
+      expected_sent: "rotate_stop"
+    },
+    {
+      left_key: "down",
+      right_key: "up",
+      event: "left",
+      expected_sent: "rotate_stop"
+    },
+    {
+      left_key: "up",
+      right_key: "up",
+      event: "left",
+      expected_sent: null
+    },
+    {
+      left_key: "up",
+      right_key: "down",
+      event: "left",
+      expected_sent: null
+    },
+    {
+      left_key: "up",
+      right_key: "up",
+      event: "right",
+      expected_sent: null
+    },
+    {
+      left_key: "down",
+      right_key: "up",
+      event: "right",
+      expected_sent: null
+    }
+  ];
+  up_sent_tests.forEach(function(test_conditions) {
+    describe(`When left key is ${test_conditions.left_key}`, () => {
+      describe(` and right key is ${test_conditions.right_key}`, () => {
+        describe(` and we receive ${test_conditions.event}`, () => {
+          if (test_conditions.expected_sent) {
+            it(`sends ${test_conditions.expected_sent}`, () => {
+              // expect(game_server.send).toHaveBeenCalledWith(test_conditions.expected_sent);
+            });
+          } else {
+            it("does not send", () => {
+              // expect(game_server.send).not.toHaveBeenCalled();
+            });
+          }
+        });
+      });
+    });
+  });
+  const down_sent_tests = [
+    {
+      left_key: "up",
+      right_key: "up",
+      event: "left",
+      expected_sent: "rotate_left"
+    },
+    {
+      left_key: "up",
+      right_key: "up",
+      event: "right",
+      expected_sent: "rotate_right"
+    },
+    {
+      left_key: "up",
+      right_key: "down",
+      event: "left",
+      expected_sent: "rotate_stop"
+    },
+    {
+      left_key: "down",
+      right_key: "up",
+      event: "right",
+      expected_sent: "rotate_stop"
+    },
+    {
+      left_key: "up",
+      right_key: "down",
+      event: "right",
+      expected_sent: null
+    },
+    {
+      left_key: "down",
+      right_key: "up",
+      event: "left",
+      expected_sent: null
+    },
+    {
+      left_key: "down",
+      right_key: "down",
+      event: "left",
+      expected_sent: null
+    },
+    {
+      left_key: "down",
+      right_key: "down",
+      event: "right",
+      expected_sent: null
+    }
+  ];
+  down_sent_tests.forEach(function(test_conditions) {
+    describe(`When left key is ${test_conditions.left_key}`, () => {
+      describe(` and right key is ${test_conditions.right_key}`, () => {
+        describe(` and we receive ${test_conditions.event}`, () => {
+          if (test_conditions.expected_sent) {
+            it(`sends ${test_conditions.expected_sent}`, () => {
+              // expect(game_server.send).toHaveBeenCalledWith(test_conditions.expected_sent);
+            });
+          } else {
+            it("does not send", () => {
+              // expect(game_server.send).not.toHaveBeenCalled();
+            });
+          }
+        });
+      });
+    });
+  });
+  const up_state_tests = [
+    {
+      left_key: "up",
+      right_key: "up",
+      event: "left",
+      expected_left_key_state: "up",
+      expected_right_key_state: "up"
+    },
+    {
+      left_key: "up",
+      right_key: "up",
+      event: "right",
+      expected_left_key_state: "up",
+      expected_right_key_state: "up"
+    },
+    {
+      left_key: "up",
+      right_key: "down",
+      event: "left",
+      expected_left_key_state: "up",
+      expected_right_key_state: "down"
+    },
+    {
+      left_key: "up",
+      right_key: "down",
+      event: "right",
+      expected_left_key_state: "up",
+      expected_right_key_state: "up"
+    },
+    {
+      left_key: "down",
+      right_key: "up",
+      event: "left",
+      expected_left_key_state: "up",
+      expected_right_key_state: "up"
+    },
+    {
+      left_key: "down",
+      right_key: "up",
+      event: "right",
+      expected_left_key_state: "down",
+      expected_right_key_state: "up"
+    },
+    {
+      left_key: "down",
+      right_key: "down",
+      event: "left",
+      expected_left_key_state: "up",
+      expected_right_key_state: "down"
+    },
+    {
+      left_key: "down",
+      right_key: "down",
+      event: "right",
+      expected_left_key_state: "down",
+      expected_right_key_state: "up"
+    }
+  ];
+  up_state_tests.forEach(function(test_conditions) {
+    describe(`When left key is ${test_conditions.left_key}`, () => {
+      describe(` and right key is ${test_conditions.right_key}`, () => {
+        describe(` and we receive ${test_conditions.event}`, () => {
+          it(`right key is ${test_conditions.expected_right_key_state}`, () => {
+            // expect(keyboard.right_key).toEqual(test_conditions.expected_right_key_state);
+          });
+          it(`left key is ${test_conditions.expected_left_key_state}`, () => {
+            // expect(keyboard.left_key).toEqual(test_conditions.expected_left_key_state);
+          });
+        });
+      });
+    });
+  });
+  const down_state_tests = [
+    {
+      left_key: "up",
+      right_key: "up",
+      event: "left",
+      expected_left_key_state: "down",
+      expected_right_key_state: "up"
+    },
+    {
+      left_key: "up",
+      right_key: "up",
+      event: "right",
+      expected_left_key_state: "up",
+      expected_right_key_state: "down"
+    },
+    {
+      left_key: "up",
+      right_key: "down",
+      event: "left",
+      expected_left_key_state: "down",
+      expected_right_key_state: "down"
+    },
+    {
+      left_key: "up",
+      right_key: "down",
+      event: "right",
+      expected_left_key_state: "up",
+      expected_right_key_state: "down"
+    },
+    {
+      left_key: "down",
+      right_key: "up",
+      event: "left",
+      expected_left_key_state: "down",
+      expected_right_key_state: "up"
+    },
+    {
+      left_key: "down",
+      right_key: "up",
+      event: "right",
+      expected_left_key_state: "down",
+      expected_right_key_state: "down"
+    },
+    {
+      left_key: "down",
+      right_key: "down",
+      event: "left",
+      expected_left_key_state: "down",
+      expected_right_key_state: "down"
+    },
+    {
+      left_key: "down",
+      right_key: "down",
+      event: "right",
+      expected_left_key_state: "down",
+      expected_right_key_state: "down"
+    }
+  ];
+  down_state_tests.forEach(function(test_conditions) {
+    describe(`When left key is ${test_conditions.left_key}`, () => {
+      describe(` and right key is ${test_conditions.right_key}`, () => {
+        describe(` and we receive ${test_conditions.event}`, () => {
+          it(`right key is ${test_conditions.expected_right_key_state}`, () => {
+            // expect(keyboard.right_key).toEqual(test_conditions.expected_right_key_state);
+          });
+          it(`left key is ${test_conditions.expected_left_key_state}`, () => {
+            // expect(keyboard.left_key).toEqual(test_conditions.expected_left_key_state);
+          });
+        });
+      });
+    });
+  });
+  describe("we receive clone_key_down", () => {
+    it("sends 'clone'", () => {
+      // expect(game_server.send).toHaveBeenCalledWith('clone');
+    });
+  });
 });
+
+
+
+
+
+
+
+
+
+
+
 
 // (function() {
 //   describe("ShipCommandController", function() {
