639b99659086af1ee50d02cfb960b341f5f0c89b
John Maxwell <jmax@jmaxhome.com>
2023-06-08 20:34:22 -0400

    Moved dispatch_message, on_error, and on_close into Application

--- react-client/src/application.js
+++ react-client/src/application.js
@@ -1,2 +1,50 @@
-export default class Application{
+import Message from "./message";
+
+export default class Application {
+  constructor(game_server) {
+    this._game_server = game_server;
+  }
+
+  createWebsocket() {
+    try {
+      this._socket = new WebSocket(`ws://${window.location.host}/engine.io/?EIO=3&transport=websocket`);
+
+      this._socket.onopen = (event) => {
+        this._socket.onmessage = this.dispatch_message;
+      };
+
+      this._socket.onerror = this.on_error;
+      this._socket.onclose = this.on_close;
+      return this._socket;
+    } catch (e) {
+      console.log(`error: ${e}`);
+    }
+  }
+
+  socket() {
+    return this._socket;
+  }
+
+  dispatch_message(the_message) {
+    const message = new Message(the_message)
+
+    switch (message.type()) {
+      case '0':
+        this._game_server.sid(JSON.parse(message.payload()).sid);
+        console.log("got sid: ", this._game_server.sid());
+        break;
+
+      case '4':
+        this._game_server.render_gameboard(message);
+        break;
+    }
+  }
+
+  on_error(error) {
+    console.log("error: ", error);
+  }
+
+  on_close(event) {
+    console.log("close:", event);
+  }
 }
--- react-client/src/index.js
+++ react-client/src/index.js
@@ -3,50 +3,13 @@ import './index.css';
 import GameServer from './game_server.js';
 import {Keyboard} from './keyboard.js';
 import Message from './message'
+import Application from "./application";
 
-function createWebsocket() {
-  try {
-    const new_socket = new WebSocket(`ws://${window.location.host}/engine.io/?EIO=3&transport=websocket`);
-
-    new_socket.onopen = (event) => {
-      new_socket.onmessage = dispatch_message;
-    };
-
-    new_socket.onerror = on_error;
-    new_socket.onclose = on_close;
-    return new_socket;
-  } catch (e) {
-    console.log(`error: ${e}`);
-  }
-}
-
-let exampleSocket = createWebsocket();
-const game_server = new GameServer(exampleSocket);
+let application = new Application();
+application.createWebsocket(on_close,on_error);
+const game_server = new GameServer(application.socket());
 const keyboard_state = new Keyboard(game_server);
 
-function on_error(error) {
-  console.log("error: ", error);
-};
-
-function on_close(event) {
-  console.log("close:", event);
-};
-
-
-function dispatch_message(the_message) {
-  const message = new Message(the_message)
-
-  switch (message.type() ) {
-    case '0':
-      game_server.sid( JSON.parse(message.payload()).sid);
-      console.log("got sid: ", game_server.sid());
-      break;
-
-    case '4':
-      game_server.render_gameboard(message);
-      break;
-  }
-};
 
 
 function onKeyDown(event) {
