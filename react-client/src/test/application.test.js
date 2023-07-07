eade238b9da393f81398a52611348927f5addf5d
Jeff Waltzer <jeffwaltzer@gmail.com>
2023-07-06 20:07:31 -0400

    refactored test

--- react-client/src/test/application.test.js
+++ react-client/src/test/application.test.js
@@ -1,34 +1,37 @@
 import Application from '../application';
 
-
 const context_for_spyon = {
   FakeWebSocket: () => {
   }
 };
 
 describe('Application', () => {
+  let fake_document;
+  let application;
+
+  beforeEach(() => {
+    fake_document = {}
+    application = () => new Application(fake_document, context_for_spyon.FakeWebSocket);
+  })
+
   it("can be instantiated", () => {
-    let fake_document = {};
     expect(new Application(fake_document, context_for_spyon.FakeWebSocket)).toBeDefined();
   });
 
   describe('#run', () => {
+
     it('catches errors', () => {
-      let fake_document = {};
-      const application = new Application(fake_document, context_for_spyon.FakeWebSocket);
-      jest.spyOn(application, 'build_websocket').mockImplementation(()=> {
+      jest.spyOn(application(), 'build_websocket').mockImplementation(() => {
         throw 'hell';
       })
-      expect(() => application.run()).not.toThrow('hell')
+      expect(() => application().run()).not.toThrow('hell')
     });
   });
 
   describe('#build_websocket', () => {
     it("creates a websocket", () => {
-      let fake_document = {};
       jest.spyOn(context_for_spyon, 'FakeWebSocket');
-      (new Application(fake_document, context_for_spyon.FakeWebSocket)).build_websocket();
-
+      application().build_websocket();
       expect(context_for_spyon.FakeWebSocket).toHaveBeenCalled();
     });
   });
