a574528d3de7a1b2324d885a03b2b3b11126bac6
Jeff Waltzer <jeffwaltzer@gmail.com>
2023-10-12 20:50:51 -0400

    Fix score color

--- react-client/src/GTag.js
+++ react-client/src/GTag.js
@@ -7,13 +7,15 @@ function GTag(props) {
     let items = polygon.wireframe.map((wf, index) => {
       return <polygon key={index} points={makePolygonPoints(wf)} stroke={wf.color}/>;
     })
-    if (polygon.score > 0)
+    if (polygon.score > 0) {
+      const score_color=polygon.wireframe[0].color;
       items.push(<text
-          key={`score-${index}`}
-          x={polygon.position[0]}
-          y={polygon.position[1]+20}
+        key={`score-${index}`}
+        x={polygon.position[0]}
+        y={polygon.position[1] + 20}
+        stroke={score_color}
       >{polygon.score}</text>);
-
+    }
     return (<g>{items}</g>);
 }
 
