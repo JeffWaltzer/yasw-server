3b5bdc51363d38fe375bdffe8db864d28622bbc0
Jeff Waltzer <jeffwaltzer@gmail.com>
2023-12-28 20:49:38 -0500

    Handle thrust button up

--- react-client/src/gamePad.js
+++ react-client/src/gamePad.js
@@ -1,7 +1,27 @@
 export default class GamePad {
 
-  last_gamepad_state= {
-      fire: () => {},
-      thrust: () => {},
+  constructor(socket) {
+    this._socket=socket;
+    this._thrust=false;
+    this._last_gamepad_state =
+      {
+        thrust: false,
+      };
+  }
+
+  interpret_command(new_gamepad) {
+    if (this._last_gamepad_state && this._last_gamepad_state.thrust && !new_gamepad.thrust())
+      this.command_socket().send("{\"command\":\"thrust_off\"}")
+    this._last_gamepad_state.thrust = new_gamepad.thrust();
+  }
+
+  command_socket() {
+    return this._socket;
+  }
+
+  thrust(new_value=undefined) {
+    if (new_value !== undefined)
+      this._thrust = new_value;
+    return this._thrust;
   }
 }
