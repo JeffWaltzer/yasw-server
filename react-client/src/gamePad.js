9cbb86ab55f968b2e8a8619f6fbea56b1b428dcc
Jeff Waltzer <jeffwaltzer@gmail.com>
2024-05-09 20:56:34 -0400

    extracted methods for clarity

--- react-client/src/gamePad.js
+++ react-client/src/gamePad.js
@@ -8,27 +8,51 @@ export default class GamePad {
   }
 
   interpret_command(new_gamepad_state) {
-    if (this._old_gamepad_state.thrust() && !new_gamepad_state.thrust())
+    if (this.stop_thrust(new_gamepad_state))
       this.sendCommand("{\"command\":\"thrust_off\"}");
 
-    if (!this._old_gamepad_state.thrust() && new_gamepad_state.thrust())
+    if (this.start_thrust(new_gamepad_state))
       this.sendCommand("{\"command\":\"thrust_on\"}")
 
-    if (!this._old_gamepad_state.fire() && new_gamepad_state.fire())
+    if (this.fire(new_gamepad_state))
       this.sendCommand("{\"command\":\"fire\"}");
 
-    if (!this._old_gamepad_state.rotating_left() && new_gamepad_state.rotating_left())
+    if (this.start_rotating_left(new_gamepad_state))
       this.sendCommand("{\"command\":\"rotate_left\"}");
 
-    if (!this._old_gamepad_state.rotating_right() && new_gamepad_state.rotating_right())
+    if (this.start_rotating_right(new_gamepad_state))
       this.sendCommand("{\"command\":\"rotate_right\"}");
 
-    if (!this._old_gamepad_state.stopped() && new_gamepad_state.stopped())
+    if (this.stop_rotating(new_gamepad_state))
       this.sendCommand("{\"command\":\"rotate_stop\"}");
 
     this._old_gamepad_state = new_gamepad_state;
   }
 
+
+  stop_rotating(new_gamepad_state) {
+    return !this._old_gamepad_state.stopped() && new_gamepad_state.stopped();
+  }
+
+  start_rotating_right(new_gamepad_state) {
+    return !this._old_gamepad_state.rotating_right() && new_gamepad_state.rotating_right();
+  }
+
+  start_rotating_left(new_gamepad_state) {
+    return !this._old_gamepad_state.rotating_left() && new_gamepad_state.rotating_left();
+  }
+
+  fire(new_gamepad_state) {
+    return !this._old_gamepad_state.fire() && new_gamepad_state.fire();
+  }
+
+  start_thrust(new_gamepad_state) {
+    return !this._old_gamepad_state.thrust() && new_gamepad_state.thrust();
+  }
+
+  stop_thrust(new_gamepad_state) {
+    return this._old_gamepad_state.thrust() && !new_gamepad_state.thrust();
+  }
 
   sendCommand(command) {
     this.command_socket().send(command)
