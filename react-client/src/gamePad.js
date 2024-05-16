81c50194259209b06c817c4aed9e15de284b3dad
John Maxwell <jmax@jmaxhome.com>
2024-05-16 19:21:59 -0400

    Extracted maybe_send

--- react-client/src/gamePad.js
+++ react-client/src/gamePad.js
@@ -7,15 +7,15 @@ export default class GamePad {
     this._old_gamepad_state = new GamePadState({});
   }
 
+  maybe_send(new_gamepad_state, command) {
+    if (this[command](new_gamepad_state))
+      this.sendCommand(`{\"command\":\"${command}\"}`);
+  }
+
   interpret_command(new_gamepad_state) {
-    if (this.stop_thrust(new_gamepad_state))
-      this.sendCommand("{\"command\":\"thrust_off\"}");
-
-    if (this.start_thrust(new_gamepad_state))
-      this.sendCommand("{\"command\":\"thrust_on\"}")
-
-    if (this.fire(new_gamepad_state))
-      this.sendCommand("{\"command\":\"fire\"}");
+    this.maybe_send(new_gamepad_state, 'thrust_off');
+    this.maybe_send(new_gamepad_state, 'thrust_on');
+    this.maybe_send(new_gamepad_state, 'fire');
 
     if (this.start_rotating_left(new_gamepad_state))
       this.sendCommand("{\"command\":\"rotate_left\"}");
@@ -46,11 +46,11 @@ export default class GamePad {
     return !this._old_gamepad_state.fire() && new_gamepad_state.fire();
   }
 
-  start_thrust(new_gamepad_state) {
+  thrust_on(new_gamepad_state) {
     return !this._old_gamepad_state.thrust() && new_gamepad_state.thrust();
   }
 
-  stop_thrust(new_gamepad_state) {
+  thrust_off(new_gamepad_state) {
     return this._old_gamepad_state.thrust() && !new_gamepad_state.thrust();
   }
 
