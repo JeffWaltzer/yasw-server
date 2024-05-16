a1c544ffc94c30e4d775d25e76aaa148208e8359
John Maxwell <jmax@jmaxhome.com>
2024-05-16 19:43:05 -0400

    Make interpret_command completely table-driven

--- react-client/src/gamePad.js
+++ react-client/src/gamePad.js
@@ -7,45 +7,51 @@ export default class GamePad {
     this._old_gamepad_state = new GamePadState({});
   }
 
-  maybe_send(new_gamepad_state, command) {
-    if (this[command](new_gamepad_state))
-      this.sendCommand(`{\"command\":\"${command}\"}`);
-  }
-
   interpret_command(new_gamepad_state) {
-    this.maybe_send(new_gamepad_state, 'thrust_off');
-    this.maybe_send(new_gamepad_state, 'thrust_on');
-    this.maybe_send(new_gamepad_state, 'fire');
-    this.maybe_send(new_gamepad_state, 'rotate_left');
-    this.maybe_send(new_gamepad_state, 'rotate_right');
-    this.maybe_send(new_gamepad_state, 'rotate_stop');
+    this._new_gamepad_state = new_gamepad_state;
+
+    [
+      'thrust_off',
+      'thrust_on',
+      'fire',
+      'rotate_left',
+      'rotate_right',
+      'rotate_stop',
+    ].forEach((command) => {
+      this.maybe_send(command);
+    });
 
     this._old_gamepad_state = new_gamepad_state;
   }
 
 
-  rotate_stop(new_gamepad_state) {
-    return !this._old_gamepad_state.stopped() && new_gamepad_state.stopped();
+  maybe_send(command) {
+    if (this[command]())
+      this.sendCommand(`{\"command\":\"${command}\"}`);
   }
 
-  rotate_right(new_gamepad_state) {
-    return !this._old_gamepad_state.rotating_right() && new_gamepad_state.rotating_right();
+  rotate_stop() {
+    return !this._old_gamepad_state.stopped() && this._new_gamepad_state.stopped();
   }
 
-  rotate_left(new_gamepad_state) {
-    return !this._old_gamepad_state.rotating_left() && new_gamepad_state.rotating_left();
+  rotate_right() {
+    return !this._old_gamepad_state.rotating_right() && this._new_gamepad_state.rotating_right();
   }
 
-  fire(new_gamepad_state) {
-    return !this._old_gamepad_state.fire() && new_gamepad_state.fire();
+  rotate_left() {
+    return !this._old_gamepad_state.rotating_left() && this._new_gamepad_state.rotating_left();
   }
 
-  thrust_on(new_gamepad_state) {
-    return !this._old_gamepad_state.thrust() && new_gamepad_state.thrust();
+  fire() {
+    return !this._old_gamepad_state.fire() && this._new_gamepad_state.fire();
   }
 
-  thrust_off(new_gamepad_state) {
-    return this._old_gamepad_state.thrust() && !new_gamepad_state.thrust();
+  thrust_on() {
+    return !this._old_gamepad_state.thrust() && this._new_gamepad_state.thrust();
+  }
+
+  thrust_off() {
+    return this._old_gamepad_state.thrust() && !this._new_gamepad_state.thrust();
   }
 
   sendCommand(command) {
