180c97b5026269be5e9e5b7162e6a1fe7698e1c2
John Maxwell <jmax@jmaxhome.com>
2025-02-04 19:34:27 -0500

    Un-coffeed a spec.

---
+++ spec/connect_ship_socket_spec.js
@@ -0,0 +1,34 @@
+var ship, yasw;
+
+yasw = require('./../src/yasw_server');
+
+ship = require('./../src/ship');
+
+describe("ship#outline", function() {
+  var fake_socket, on_callbacks, server, the_ship;
+  server = null;
+  on_callbacks = {};
+  fake_socket = null;
+  the_ship = null;
+  beforeEach(function() {
+    fake_socket = {
+      send: function() {},
+      on: function(event, callback) {
+        return on_callbacks[event] = callback;
+      }
+    };
+    server = yasw.createServer();
+    the_ship = server.on_new_websocket(fake_socket);
+    on_callbacks['message'](JSON.stringify({
+      command: 'rotate_left'
+    }));
+  });
+
+  it('rotates the correct ship', function() {
+    expect(the_ship.angular_velocity()).toEqual(-ship.Ship.rotation_rate);
+  });
+
+  return afterEach(function() {
+    server = null;
+  });
+});
